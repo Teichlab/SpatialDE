029cd602a79e51d4fa38654efe50a8f1d9ebfb4d
Valentine Svensson <vale@ebi.ac.uk>
2016-11-25 13:22:20 +0000

    Added script for fitting GPs

--- split_data.py
+++ split_data.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 pattern = 'http://www.spatialtranscriptomicsresearch.org/wp-content/uploads/2016/07/Rep{}_MOB_count_matrix-1.tsv'
 
-NUM_NODES = 10
+GENES_PER_NODE = 500
 replicates = [11, 12]
 
 for replicate in replicates:
     df = pd.read_table(pattern.format(replicate), index_col=0)
 
-    for grp, df_part in df.T.groupby(np.arange(df.shape[1]) // (df.shape[1] // NUM_NODES)):
+    for grp, df_part in df.T.groupby(np.arange(df.shape[1]) // GENES_PER_NODE):
         df_part.T.to_csv('data/Rep{}_MOB_{}.csv'.format(replicate, grp))
