2fb6576c4b39289e0d3d8fdd12efa55a1e9272ca
Valentine Svensson <v@nxn.se>
2017-01-05 06:08:00 +0000

    Included a file for making visualizations

---
+++ visualization.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+fgp = __import__('FaST-GP')
+
+def ls_sample_2d(ls_list=[3, 10, 30, 100]):
+    x = np.linspace(0, 200)
+    X1, X2 = np.meshgrid(x, x)
+    X = np.vstack((X1.flatten(), X2.flatten())).T
+
+    for i, ls in enumerate(ls_list):
+        K = fgp.SE_kernel(X, ls)
+        y = np.random.multivariate_normal(0 * X[:, 0], K)
+
+        plt.subplot(1, len(ls_list), i + 1)
+        plt.pcolormesh(X1, X2, y.reshape(X1.shape), cmap=cm.inferno)
+        plt.contour(X1, X2, y.reshape(X1.shape), cmap=cm.inferno)
+        plt.axis('equal')
+        plt.title('$ \ell = {} $'.format(ls))
+
+
+    fig = plt.gcf()
+    fig.set_size_inches(9, 3)
+    plt.savefig('ls_guide.png')
+
+
+if __name__ == '__main__':
+    ls_sample_2d()
