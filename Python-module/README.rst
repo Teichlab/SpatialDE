a2becc6560570060c98604c6b03ec4e3d51be2e3
Valentine Svensson <vale@ebi.ac.uk>
2017-03-03 15:44:26 +0000

    Updated python distribution settings

---
+++ Python-module/README.rst
@@ -0,0 +1,6 @@
+SpatialDE
+=========
+
+Perform non-linear differential expression tests in
+spatially or temporally  resolved gene expression data.
+
--- Python-module/setup.py
+++ Python-module/setup.py
@@ -4,8 +4,9 @@ setup(
         name='SpatialDE',
         version='0.2.0',
         description='Spatial and Temporal DE test',
+        url='https://github.com/Teichlab/SpatialGP',
         packages=find_packages(),
-        install_requires=['numpy', 'pandas', 'tqdm'],
+        install_requires=['numpy', 'scipy', 'pandas', 'tqdm'],
         author='Valentine Svensson',
         author_email='valentine@nxn.se',
         license='MIT'
