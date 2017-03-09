7c8fdd51a5ca1ce328d98a9d38031062527d67cf
Valentine Svensson <vale@ebi.ac.uk>
2017-03-09 16:22:23 +0000

    Dont consider PER models during DE test

--- Python-module/SpatialDE/base.py
+++ Python-module/SpatialDE/base.py
@@ -327,7 +327,6 @@ def run(X, exp_tab, kernel_space=None, pvalues=True, null_model_samples=10000):
     if kernel_space == None:
         l_min, l_max = get_l_limits(X)
         kernel_space = {
-            'PER': np.logspace(np.log10(l_min), np.log10(l_max), 10),
             'SE': np.logspace(np.log10(l_min), np.log10(l_max), 10),
             'const': 0
         }
