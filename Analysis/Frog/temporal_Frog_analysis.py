771384913507e002f3d41d0f307722e27958df6f
Valentine Svensson <vale@ebi.ac.uk>
2017-03-23 17:32:30 +0000

    Change Frog analysis to regress our ERCC and num_genes

--- Analysis/Frog/temporal_Frog_analysis.py
+++ Analysis/Frog/temporal_Frog_analysis.py
@@ -1,25 +1,28 @@
 import numpy as np
 import pandas as pd
 
-import SpatialDE as sde
+import NaiveDE
+import SpatialDE
 
 def main():
-    df = pd.read_csv('data/GSE65785_clutchApolyA_relative_TPM.csv', index_col=0)
-    df = df[df.sum(1) >= 3]  # Filter practically unobserved genes
-
     # Get time points for each sample
-    sample_info = pd.read_csv('data/sample_info.csv', index_col=0)
+    sample_info = pd.read_csv('Frog_sample_info.csv', index_col=0)
+
+    # Load expression
+    df = pd.read_csv('data/GSE65785_clutchApolyA_relative_TPM.csv', index_col=0)
+    df = df[sample_info.index]
+    df = df[df.sum(1) >= 3]  # Filter practically unobserved genes
 
     X = sample_info[['hpf']]
 
     # Convert expression data to log scale, with genes in columns
-    dfm = np.log10(df + 1).T
+    dfm = NaiveDE.stabilize(df)
+    res = NaiveDE.regress_out(sample_info, dfm, 'np.log(ERCC) + np.log(num_genes)', rcond=1e-4).T
 
     # Perform Spatial DE test with default settings
-    results = sde.run(X, dfm)
+    results = SpatialDE.run(X, res)
 
     # Save results and annotation in files for interactive plotting and interpretation
-    sample_info.to_csv('Frog_sample_info.csv')
     results.to_csv('Frog_final_results.csv')
 
     return results 
