13f88b63c30fb1adb0c544876627a8459d220d6a
Valentine Svensson <vale@ebi.ac.uk>
2017-10-03 10:01:56 +0100

    Started R package

---
+++ R-package/SpatialDE/R/base.R
@@ -0,0 +1,20 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+SE_kernel <- function(X, l) {
+  R2 <- as.matrix(dist(X) ** 2)
+  K <- exp(-R2 / (2 * l ** 2))
+  K
+}
