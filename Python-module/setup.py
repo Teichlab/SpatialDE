4ceadde19fea154897e6af5f6dd0e9edc4fb3f25
Valentine Svensson <vale@ebi.ac.uk>
2017-11-16 18:24:47 +0000

    Upped version for PyPI

--- Python-module/setup.py
+++ Python-module/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='SpatialDE',
-        version='0.4.0',
+        version='0.5.0',
         description='Spatial and Temporal DE test',
         url='https://github.com/Teichlab/SpatialDE',
         packages=find_packages(),
