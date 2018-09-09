23ec19131473bf87d5fb3b17508aae430f2e6310
Valentine Svensson <valentine@nxn.se>
2018-09-08 22:46:29 -0700

    Increased version number

--- Python-module/setup.py
+++ Python-module/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='SpatialDE',
-        version='1.0.0',
+        version='1.1.0',
         description='Spatial and Temporal DE test',
         url='https://github.com/Teichlab/SpatialDE',
         packages=find_packages(),
