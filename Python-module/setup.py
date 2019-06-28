f80323218dbb821b493affcbbfc196003859303c
Philipp A <flying-sheep@web.de>
2019-06-28 11:23:08 +0200

    Add long_description to setup.py

--- Python-module/setup.py
+++ Python-module/setup.py
@@ -1,19 +1,24 @@
+from pathlib import Path
 from setuptools import setup, find_packages
 
+HERE = Path(__file__).parent
+
 setup(
-        name='SpatialDE',
-        version='1.1.1',
-        description='Spatial and Temporal DE test',
-        url='https://github.com/Teichlab/SpatialDE',
-        packages=find_packages(),
-        include_package_data=True,
-        install_requires=['numpy', 'scipy', 'pandas>=0.23', 'tqdm',
-                          'NaiveDE', 'Click'],
-        entry_points='''
-            [console_scripts]
-            spatialde=SpatialDE.scripts.spatialde_cli:main
-        ''',
-        author='Valentine Svensson',
-        author_email='valentine@nxn.se',
-        license='MIT'
-    )
+    name='SpatialDE',
+    version='1.1.1',
+    description='Spatial and Temporal DE test',
+    long_description=(HERE.parent / 'README.rst').read_text(),
+    url='https://github.com/Teichlab/SpatialDE',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'numpy', 'scipy', 'pandas>=0.23', 'tqdm',
+        'NaiveDE', 'Click',
+    ],
+    entry_points=dict(
+        console_scripts=['spatialde=SpatialDE.scripts.spatialde_cli:main'],
+    ),
+    author='Valentine Svensson',
+    author_email='valentine@nxn.se',
+    license='MIT',
+)
