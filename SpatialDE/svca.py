81a7038e538b58f4ded11181de00d32e1e9d9e18
Ilia Kats <ilia-kats@gmx.net>
2020-10-19 09:26:40 +0200

    svca: use squared exponential kernel (faster)

--- SpatialDE/svca.py
+++ SpatialDE/svca.py
@@ -12,54 +12,30 @@ from gpflow.utilities import to_default_float
 import tensorflow as tf
 import tensorflow_probability as tfp
 
-from ._internal.sm_kernel import Spectral, SpectralMixture
 from ._internal.svca import SVCA, SVCAInteractionScoreTest
 from ._internal.optimizer import MultiScipyOptimizer
 from ._internal.util import get_l_limits, bh_adjust, calc_sizefactors
 from ._internal.distance_cache import DistanceCache
 
 
-def _make_kernel(ncomponents, ard, l_min, l_max):
-    kernels = []
-    for l in np.geomspace(l_min, l_max, ncomponents):
-        if ard:
-            lengthscales = [l] * X.shape[1]
-            periods = [l_max] * X.shape[1]
-        else:
-            lengthscales = l
-            periods = l_max
-        k = Spectral(variance=1, lengthscales=lengthscales, periods=periods)
-        gpflow.set_trainable(k.variance, False)
-        k.lengthscales.transform = tfp.bijectors.Sigmoid(
-            low=to_default_float(0.5 * l_min), high=to_default_float(2 * l_max)
-        )
-        k.periods.transform = tfp.bijectors.Sigmoid(
-            low=to_default_float(0.5 * l_min), high=to_default_float(2 * l_max)
-        )
-        kernels.append(k)
-    return SpectralMixture(kernels)
-
-
 def test_spatial_interactions(
     adata: AnnData,
     spatial_key: str = "spatial",
-    ncomponents: int = 1,
     ard: bool = False,
     sizefactors: Optional[np.ndarray] = None,
     copy: bool = False,
 ) -> Tuple[pd.DataFrame, Union[AnnData, None]]:
-    if ncomponents < 1:
-        warnings.warn(
-            f"ncomponents must be 1 or larger, but received {ncomponents}. Setting ncomponents=1"
-        )
-        ncomponents = 1
-
     if sizefactors is None:
         sizefactors = calc_sizefactors(adata)
 
     X = adata.obsm[spatial_key]
     l_min, l_max = get_l_limits(DistanceCache(X))
-    kernel = _make_kernel(ncomponents, ard, l_min, l_max)
+    lscales = l_min if not ard else [l_min] * X.shape[1]
+    kernel = gpflow.kernels.SquaredExponential(lengthscales=lscales)
+    kernel.lengthscales.transform = tfp.bijectors.Sigmoid(
+        low=to_default_float(0.5 * l_min), high=to_default_float(2 * l_max)
+    )
+    gpflow.set_trainable(kernel.variance, False)
 
     results = []
     parameters = []
@@ -90,7 +66,6 @@ def test_spatial_interactions(
         toreturn = None
     adata.varm["svca"] = np.array(parameters, dtype=dtype)
     adata.obsm["svca_sizefactors"] = sizefactors
-    adata.uns["svca_ncomponents"] = ncomponents
     adata.uns["svca_ard"] = ard
 
     return results, toreturn
@@ -100,14 +75,12 @@ def fit_spatial_interactions(
     adata: AnnData,
     genes: Optional[List[str]],
     spatial_key: str = "spatial",
-    ncomponents: int = 1,
     ard: bool = False,
     sizefactors: Optional[np.ndarray] = None,
 ) -> pd.DataFrame:
     if (
         "svca" not in adata.varm
         or "svca_sizefactors" not in adata.obsm
-        or "svca_ncomponents" not in adata.uns
         or "svca_ard" not in adata.uns
     ):
         warnings.warn("SVCA parameters not found in adata. Performing ab initio fitting.")
@@ -115,11 +88,6 @@ def fit_spatial_interactions(
             genes = adata.var_names
         if sizefactors is None:
             sizefactors = calc_sizefactors(adata)
-        if ncomponents < 1:
-            warnings.warn(
-                f"ncomponents must be 1 or larger, but received {ncomponents}. Setting ncomponents=1"
-            )
-            ncomponents = 1
         trainable = True
     else:
         if genes is None:
@@ -127,12 +95,16 @@ def fit_spatial_interactions(
             genes = adata.var_names
         sizefactors = adata.obsm["svca_sizefactors"]
         ard = adata.uns["svca_ard"]
-        ncomponents = adata.uns["svca_ncomponents"]
         trainable = False
 
     X = adata.obsm[spatial_key]
     l_min, l_max = get_l_limits(DistanceCache(X))
-    kernel = _make_kernel(ncomponents, ard, l_min, l_max)
+    lscales = l_min if not ard else [l_min] * X.shape[1]
+    kernel = gpflow.kernels.SquaredExponential(lengthscales=lscales)
+    kernel.lengthscales.transform = tfp.bijectors.Sigmoid(
+        low=to_default_float(0.5 * l_min), high=to_default_float(2 * l_max)
+    )
+    gpflow.set_trainable(kernel.variance, False)
 
     model = SVCA(adata.X, X, sizefactors, kernel)
     model.use_interactions(True)
